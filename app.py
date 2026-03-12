69088b81d8385ad85282a6213d14aea93382ebcf
Dominik Meisner <meisnerd2003@gmail.com>
2026-03-12 09:52:34 +0100

    feat: create basic app with /upload endpoint

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+
+import qrcode
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+from token_gen import generate_token
+
+load_dotenv()
+
+app = FastAPI()
+
+
+SECRET = os.environ["SECRET"].encode()
+BACKEND_URL = os.environ["URL"]
+
+
+@app.get("/upload")
+def upload():
+    filename = Path("qrcode.png")
+
+    data = f"{BACKEND_URL}/upload/{generate_token(SECRET)}"
+    img = qrcode.make(data)
+
+    with open(filename, "wb") as file:
+        img.save(file)
+
+    return FileResponse(filename)
