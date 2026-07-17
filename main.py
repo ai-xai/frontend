ded8d3061e185795c0c74bef50cb7fa45969ff83
Dominik Meisner <meisnerd2003@gmail.com>
2026-07-17 21:36:43 +0200

    feat: add port to main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import sys
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", port=8000, reload="--dev" in sys.argv)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload="--dev" in sys.argv)
